935eb33e0cdf6c195c1b9ee7c36f83180ce87248
Alexander Khokhlov <alexander.xoxlov@gmail.com>
2017-04-18 22:38:45 +0300

    Lesson 02 example was executed and added to the repo

---
+++ lesson-02-python/test_example.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.google.com")
+    driver.find_element_by_name("q").send_keys("webdriver")
+    driver.find_element_by_name("btnG").click()
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))

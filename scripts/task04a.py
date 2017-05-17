9560158d96ab96cd31173e1980b35473669b48c2
Alexander Khokhlov <alexander.xoxlov@gmail.com>
2017-05-17 10:58:21 +0300

    sel6: fixed test 1 for lesson 4

--- scripts/task04a.py
+++ scripts/task04a.py
@@ -1,8 +1,8 @@
-import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 @pytest.fixture
 def browser(request):
@@ -24,7 +24,7 @@ def do_login_admin(browser):
     browser.find_element_by_css_selector('button[name="login"]').click()
     wait.until(EC.title_is("My Store"))
     # wait to be sure the page was loaded completely
-    time.sleep(3)
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li#widget-discussions")))
 
 def run_through_menu(browser):
     # find elements from menu on the left
@@ -38,9 +38,9 @@ def run_through_menu(browser):
             # go through every submenu item and click it
             for subitem in range(len(submenu_links)):
                 submenu_links[subitem].click()
-                header = browser.find_element_by_tag_name("h1").text
+                header = browser.find_elements_by_tag_name("h1")
                 # header can be printed, but its value cannot be decoded as string - problem!
-                assert(header)
+                assert(len(header))
                 # re-find elements of submenu to keep actual handlers
                 submenu_links = browser.find_elements_by_css_selector(".docs li")
         # continue searching menu items to deal with the actual page elements
